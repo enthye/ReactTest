4ef7173fd772c00ec7568380a28b074af38cd941
enthye <37763349+enthye@users.noreply.github.com>
2018-11-14 19:27:19 +1300

    test

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export const Header = (props) => {
     return (
-        <nav className="navbar navbar-default">
-            <div className="container">
+        <nav className="navbar navbar-inverse">
+            <div className="container-fluid">
                 <div className="navbar-header">
                     <ul className="nav navbar-nav">
-                        <li><a href="#">{props.homeLink}</a></li>
+                        <li><a href="">{props.homeLink}</a></li>
                     </ul>
                 </div>
             </div>
